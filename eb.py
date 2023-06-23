b46e8f7be70e7301525535654faae164cfd0742e
Sonali1256 <137482581+Sonali1256@users.noreply.github.com>
2023-06-23 13:05:48 +0600

    Add files via upload

---
+++ eb.py
@@ -0,0 +1,45 @@
+import time
+import getpass
+# Regular Colors
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+print (yellow+"""    ___       __    __   ____  __           __            
+   /   | ____/ /___/ /  / __ )/ /___  _____/ /_____  _____
+  / /| |/ __  / __  /  / __  / / __ \/ ___/ //_/ _ \/ ___/
+ / ___ / /_/ / /_/ /  / /_/ / / /_/ / /__/ ,< /  __/ /    
+/_/  |_\__,_/\__,_/  /_____/_/\____/\___/_/|_|\___/_/     
+                                                          """)
+                                                          
+                                                          
+print ("               Miss Sonali Add blocker")
+print (yellow+"Please wait..........")
+time.sleep(1)
+
+# A simple Python program to demonstrate
+# getpass.getpass() to read security question
+import getpass
+ 
+p = getpass.getpass(prompt='passward: ')
+ 
+if p.lower() == 'sonali':
+    print('Welcome..!!!')
+
+if p.lower() == 'sonali':
+	print ("6")
+	print(yellow+"         apps linik")
+	print("         ")
+	print("         ")
+	print(yellow+"https://www.mediafire.com/file/foqupajfecg63ll/Ad_Blocker_Pro_v1.4.7_53userupload.in.apk/file")
+	print ("     ")
+	print ("     ")
+	print("☝️☝️☝️☝️☝️☝️")
+else:
+    print(red+'incorrect passeard..!!!')
+    
